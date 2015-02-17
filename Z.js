17a1403b5f4278ea716f048550921a5ebd0ea76b
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2015-02-17 16:52:07 +0100

    Implemented more error handling

--- Z.js
+++ Z.js
@@ -7,9 +7,11 @@
 Zsqrt = Math.sqrt;
 
 Math.sqrt = function(parameter) {
-    var z = Z(parameter);
-    if (z)
-        return z.sqrt();
+    var z = new Z(parameter);
+    if (z && !(parameter >= 0 )){
+        z.doSqrt(z);
+        return z;
+    }
     return Zsqrt(parameter);
 };
 
@@ -28,59 +30,65 @@ function Z (a, b) {
     var ZInRectForm = true;
     var argInRad = true;
 
-    if(!b){
-        if (typeof a === "string") {
-            var args = a.match(/\s*\d+\s*\.?\d*/g);
-            if (args.length <= 2) {
-                if (a.match(/j/gi) > 1)
-                    throw new Error("Unable to parse ComplexNumber: Why more that one 'j'?");
-                else{
-                    if (!a.match(/j/gi) && args.length === 1){
-                        Re = args[0];
-                        Im = 0;
-                    } else if (a.match(/j/gi) && args.length === 1) {
-                        Re = 0;
-                        Im = args[0]
-                    } else if (Math.abs(a.search(args[0])- a.search("j")) > Math.abs(a.search(args[1])- a.search("j"))) {
-                        Re = parseFloat(args[0]);
-                        Im = parseFloat(args[1]);
-                    } else {
-                        Re = parseFloat(args[1]);
-                        Im = parseFloat(args[0]);
+
+    var processPar = function(){
+        if(!b){
+            if (typeof a === "string") {
+                var args = a.match(/\s*\d+\s*\.?\d*/g);
+                if (args.length <= 2) {
+                    if (a.match(/j/gi) > 1)
+                        throw new Error("Unable to parse ComplexNumber: Why more that one 'j'?");
+                    else{
+                        if (!a.match(/j/gi) && args.length === 1){
+                            Re = args[0];
+                            Im = 0;
+                        } else if (a.match(/j/gi) && args.length === 1) {
+                            Re = 0;
+                            Im = args[0]
+                        } else if (Math.abs(a.search(args[0])- a.search("j")) > Math.abs(a.search(args[1])- a.search("j"))) {
+                            Re = parseFloat(args[0]);
+                            Im = parseFloat(args[1]);
+                        } else {
+                            Re = parseFloat(args[1]);
+                            Im = parseFloat(args[0]);
+                        }
                     }
+
+
+
+                } else {
+                    throw new Error("Unable to parse ComplexNumber: Too many Re and/or Im");
                 }
-
-
+            } else if (typeof a === "number") {
+                Re = a;
+                Im = 0;
+            } else if (a instanceof Z) {
+                Re = a.getRe();
+                Im = a.getIm();
+                ZInRectForm = a.isInRectForm();
+                argInRad = a.isOpInRad();
+            } else {
+                throw new Error("Unable to construct ComplexNumber object: Constructor takes either strings or number(see Documentation). \n--->created object in BAD state");
+            }
+        } else {
+            if (typeof a === "number" && typeof b === "number") {
+                Re = a;
+                Im = b;
+            } else if (typeof a === "string" && typeof b === "string") {
+                Re = parseFloat(a);
+                Im = parseFloat(b);
+            } else if (!a){
+                Re = 0;
+                Im = b;
 
             } else {
-                throw new Error("Unable to parse ComplexNumber: Too many Re and/or Im");
+                throw new Error("Unable to construct ComplexNumber object: Constructor takes either strings or numbers (see Documentation) \nobject in BAD state");
             }
-        } else if (typeof a === "number") {
-            Re = a;
-            Im = 0;
-        } else if (a instanceof Z) {
-            Re = a.getRe();
-            Im = a.getIm();
-            ZInRectForm = a.isInRectForm();
-            argInRad = a.isOpInRad();
-        } else {
-            throw new Error("Unable to construct ComplexNumber object: Constructor takes either strings or number(see Documentation). \n--->created object in BAD state");
         }
-    } else {
-        if (typeof a === "number" && typeof b === "number") {
-            Re = a;
-            Im = b;
-        } else if (typeof a === "string" && typeof b === "string") {
-            Re = parseFloat(a);
-            Im = parseFloat(b);
-        } else if (!a){
-            Re = 0;
-            Im = b;
+        //INITIALIZATION PROCESSING
+    };
 
-        } else {
-            throw new Error("Unable to construct ComplexNumber object: Constructor takes either strings or numbers (see Documentation) \nobject in BAD state");
-        }
-    }
+    processPar();
 
     this.isOpInRad = function () {
         return argInRad;
@@ -110,7 +118,7 @@ function Z (a, b) {
         this.doPow(-1);
     };
     this.getAbs = function () {
-        return Math.sqrt(Math.pow(Re, 2) + Math.pow(Im, 2));
+        return Zsqrt(Math.pow(Re, 2) + Math.pow(Im, 2));
 
     };
     this.getMod = function () {
@@ -137,12 +145,14 @@ function Z (a, b) {
         return Im;
     };
     this.divideBy = function (that) {
+        that = new Z(that);
         var mod = this.getAbs() / that.getAbs();
         var arg = this.getArg() - that.getArg();
         Re = mod * Math.cos(arg);
         Im = mod * Math.sin(arg);
     };
     this.multiplyBy = function (that) {
+        that = new Z(that);
         var mod = this.getAbs() * that.getAbs();
         var arg = this.getArg() + that.getArg();
         Re = mod * Math.cos(arg);
@@ -150,20 +160,21 @@ function Z (a, b) {
 
     };
     this.addTo = function (that) {
+        that = new Z(that);
         Re = this.getRe() + that.getRe();
         Im = this.getIm() + that.getIm();
     };
     //Not Recommended
     this.differenceFrom = function (that) {
+        that = new Z(that);
         var _Re = this.getRe() - that.getRe();
         var _Im = this.getIm() - that.getIm();
         return new Z(_Re, _Im);
     };
     this.doPow = function(power) {
-        var mod = Math.pow(this.getAbs(), power);
-        var arg = this.getArg() * power;
-        Re = mod * Math.cos(arg);
-        Im = mod * Math.sin(arg);
+        var z = Z.pow(this, power);
+        Re = z.getAbs() * Math.cos(z.getArg());
+        Im = z.getAbs() * Math.sin(z.getArg());
     };
     this.doSqrt = function () {
         this.doPow(0.5);
@@ -172,32 +183,43 @@ function Z (a, b) {
 };
 
 Z.sqrt = function (z) {
-    Z.pow(z, 0.5);
-    return z;
+    z = new Z(z);
+    return Z.pow(z, 0.5);;
 };
 
 Z.add = function (z1, z2) {
+    z1 = new Z(z1);
+    z2 = new Z(z2);
     var _Re = z1.getRe() + z2.getRe();
     var _Im = z1.getIm() + z2.getIm();
     return new Z(_Re, _Im);
 };
 Z.substract = function (z1, z2) {
+    z1 = new Z(z1);
+    z2 = new Z(z2);
     var _Re = z1.getRe() - z2.getRe();
     var _Im = z1.getIm() - z2.getIm();
     return new Z(_Re, _Im);
 };
 
 Z.divide = function (z1,z2) {
+    z1 = new Z(z1);
+    z2 = new Z(z2);
     var mod = z1.getAbs() / z2.getAbs();
     var arg = z1.getArg() - z2.getArg();
     return new Z(mod * Math.cos(arg), mod * Math.sin(arg));
 };
 Z.multiply = function (z1,z2) {
+    z1 = new Z(z1);
+    z2 = new Z(z2);
     var mod = z1.getAbs() * z2.getAbs();
     var arg = z1.getArg() + z2.getArg();
     return new Z(mod * Math.cos(arg), mod * Math.sin(arg));
 };
 Z.pow = function(z, power) {
+    z = new Z(z);
+    if (typeof power !== "number")
+        throw new Error("Sorry, you can only raise to the power of real numbers");
     var mod = Math.pow(z.getAbs(), power);
     var arg = z.getArg() * power;
     return new Z(mod * Math.cos(arg), mod * Math.sin(arg));
